c1451d9ba979c13ef95d96204d3e24a583f87fe0
Lagom92 <jyyt0147@gmail.com>
2020-08-26 12:55:27 +0900

    Update models

--- WEB/student_info_pjt/infoProject/infoApp/models.py
+++ WEB/student_info_pjt/infoProject/infoApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class AiClass(models.Model):
@@ -8,7 +9,12 @@ class AiClass(models.Model):
     
 
 class AiStudent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student')
+    participate_class = models.ForeignKey(AiClass, on_delete=models.CASCADE, related_name='student')
     name = models.CharField(max_length=50)
-    class_number = models.IntegerField()
-    introduce = models.TextField()
     github = models.CharField(max_length=250)
+
+
+class StudentPost(models.Model):
+    writer = models.ForeignKey(AiStudent, on_delete=models.SET_NULL, null=True, related_name='post')
+    introduce = models.TextField()
